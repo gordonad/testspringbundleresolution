d184a2247518d7d56a83dbb78e181f8c286be80c
Glyn Normington <glyn.normington@gmail.com>
2011-12-06 14:43:18 +0000

    Add junit test and gradle build support

---
+++ src/test/java/org/springframework/testspringbundleresolution/TestSpringBundleResolution.java
@@ -0,0 +1,11 @@
+package org.springframework.testspringbundleresolution;
+
+import org.junit.Test;
+
+public class TestSpringBundleResolution {
+	
+  @Test
+  public void test() throws Exception {
+    System.out.println("Hello from a testcase!");
+  }
+}
