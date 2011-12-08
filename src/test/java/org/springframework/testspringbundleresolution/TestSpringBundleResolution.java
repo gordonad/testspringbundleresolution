0aa963ca605591b8da1faeb3afa586dcf6ca3800
Glyn Normington <glyn.normington@gmail.com>
2011-12-08 12:14:39 +0000

    First clean run

--- src/test/java/org/springframework/testspringbundleresolution/TestSpringBundleResolution.java
+++ src/test/java/org/springframework/testspringbundleresolution/TestSpringBundleResolution.java
@@ -5,7 +5,11 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
 import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.junitBundles;
 
+import javax.inject.Inject;
+
+//import javax.inject.Inject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Option;
@@ -17,23 +21,25 @@ import org.osgi.framework.BundleContext;
 @RunWith(JUnit4TestRunner.class)
 public class TestSpringBundleResolution {
 
+    @Inject
+    private BundleContext bundleContext;
+
     @Configuration
     public static Option[] configuration() throws Exception {
-        return options(mavenBundle("org.springframework", "spring-core", "3.0.6.RELEASE"));
+        return options(mavenBundle("org.springframework", "spring-core", "3.0.6.RELEASE"), junitBundles());
     }
 
     @Test
-    public void test(BundleContext bundleContext) throws Exception {
+    public void testSpringCoreIsResolvable() throws Exception {
         boolean found = false;
-        Bundle[] bundles = bundleContext.getBundles();
+        Bundle[] bundles = this.bundleContext.getBundles();
         for (Bundle bundle : bundles) {
             if ("org.springframework.core".equals(bundle.getSymbolicName())) {
                 found = true;
                 bundle.start();
-                assertTrue(true); // This fails the test with an obscure stack trace.
-                // assertEquals(Bundle.ACTIVE, bundle.getState());
+                assertEquals(Bundle.ACTIVE, bundle.getState());
             }
         }
-        // assertTrue(found);
+        assertTrue(found);
     }
 }
