a20b884be808deabaac15825d0dbe20081480526
Glyn Normington <glyn.normington@gmail.com>
2011-12-08 14:38:25 +0000

    test Spring 3.1.0.RC2

--- src/test/java/org/springframework/testspringbundleresolution/TestSpringBundleResolution.java
+++ src/test/java/org/springframework/testspringbundleresolution/TestSpringBundleResolution.java
@@ -2,14 +2,13 @@
 package org.springframework.testspringbundleresolution;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
-import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.bundle;
 import static org.ops4j.pax.exam.CoreOptions.junitBundles;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.provision;
 
 import javax.inject.Inject;
 
-//import javax.inject.Inject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Option;
@@ -21,25 +20,37 @@ import org.osgi.framework.BundleContext;
 @RunWith(JUnit4TestRunner.class)
 public class TestSpringBundleResolution {
 
+    private static final String SPRINGFRAMEWORK_MAVEN_URL = "http://maven.springframework.org/milestone";
+
+    private static final String SPRING_VERSION = "3.1.0.RC2";
+
     @Inject
     private BundleContext bundleContext;
 
     @Configuration
     public static Option[] configuration() throws Exception {
-        return options(mavenBundle("org.springframework", "spring-core", "3.0.6.RELEASE"), junitBundles());
+        return options(provisionSpringBundle("spring-core"), provisionSpringBundle("spring-beans"), junitBundles());
+    }
+
+    private static Option provisionSpringBundle(String artifactId) {
+        return provision(bundle(springBundleUrl(artifactId)).start());
+    }
+
+    private static String springBundleUrl(String artifactId) {
+        return SPRINGFRAMEWORK_MAVEN_URL + "/org/springframework/" + artifactId + "/" + SPRING_VERSION + "/" + artifactId + "-" + SPRING_VERSION
+            + ".jar";
     }
 
     @Test
-    public void testSpringCoreIsResolvable(BundleContext bc /* XXX provoke testcase failure */) throws Exception {
-        boolean found = false;
+    public void testSpringCoreIsResolvable() throws Exception {
+        int found = 0;
         Bundle[] bundles = this.bundleContext.getBundles();
         for (Bundle bundle : bundles) {
-            if ("org.springframework.core".equals(bundle.getSymbolicName())) {
-                found = true;
-                bundle.start();
+            if (bundle.getSymbolicName().startsWith("org.springframework")) {
+                found++;
                 assertEquals(Bundle.ACTIVE, bundle.getState());
             }
         }
-        assertTrue(found);
+        assertEquals(2, found);
     }
 }
